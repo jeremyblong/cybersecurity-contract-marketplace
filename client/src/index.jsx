76410bb25f2ab90f8f4537be6f0a3950d089d6af
Jeremy Blong <essentialpurposesoberliving@gmail.com>
2021-11-15 14:14:42 -0500

    finally properly wired up admin dashboard to forward facing website

---
+++ client/src/index.jsx
@@ -0,0 +1,90 @@
+import 'react-notifications/lib/notifications.css';
+import './styles/bootstrap.min.css';
+import './styles/animate.css';
+// import './styles/boxicons.min.css';
+// import './styles/flaticon.css';
+import '../node_modules/react-modal-video/css/modal-video.min.css';
+import 'react-accessible-accordion/dist/fancy-example.css';
+import './styles/style.css';
+import './styles/responsive.css';
+import 'react-phone-number-input/style.css'
+
+// components
+import React, { Fragment,useState,useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './components/app';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {BrowserRouter,Switch,Route,Redirect} from 'react-router-dom'
+import { CSSTransition,TransitionGroup } from 'react-transition-group'
+import {routes} from './route';
+import ConfigDB from './data/customizer/config'
+import { classes } from './data/layouts';
+import {NotificationContainer} from 'react-notifications';
+import Index from "./pages/index.js";
+import { RecoilRoot } from 'recoil';
+import SignIn from "./pages/sign-in.js";
+import SignUp from "./pages/sign-up.js";
+import { store } from "./redux/store/store.js";
+
+
+
+const Root = (props) =>  {
+
+  const [anim, setAnim] = useState("");
+  const animation = localStorage.getItem("animation") || ConfigDB.data.router_animation || 'fade'
+  const abortController = new AbortController();
+  const defaultLayoutObj = classes.find(item => Object.values(item).pop(1) === 'compact-wrapper');
+  const layout = localStorage.getItem('layout') ||  Object.keys(defaultLayoutObj).pop();
+
+  useEffect(() => {
+      setAnim(animation)
+      console.ignoredYellowBox = ["Warning: Each", "Warning: Failed"];
+      console.disableYellowBox = true;
+      return function cleanup() {
+          abortController.abort();
+        }
+      // eslint-disable-next-line
+    }, []);
+
+    return(
+      <Fragment>
+        <Provider store={store}>
+        <RecoilRoot>
+        <NotificationContainer />
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/sign-in" component={SignIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <App>
+            <TransitionGroup>
+              {routes.map(({ path, Component }) => (
+                  <Route key={path} exact   path={`${process.env.PUBLIC_URL}${path}`}>
+                      {({ match }) => (
+                          <CSSTransition 
+                          in={match != null}
+                          timeout={100}
+                          classNames={anim} 
+                          unmountOnExit
+                          >
+                          <div><Component/></div>
+                          </CSSTransition> 
+                      )}
+                  </Route>
+                  ))}
+            </TransitionGroup>
+          </App>
+        </Switch>
+        </BrowserRouter>
+        </RecoilRoot>
+        </Provider>
+      </Fragment>
+      )
+}
+ReactDOM.render(<Root/>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
