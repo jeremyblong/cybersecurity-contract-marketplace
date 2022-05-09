230b331907e91170092a4b0cd948622de5c7b016
Jeremy Blong <jeremyblong@gmail.com>
2022-05-09 16:58:11 -0700

    restyled 'reviews' on homepage as mobile display was weird/very long

--- client/src/components/Common/Testimonials.js
+++ client/src/components/Common/Testimonials.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import OwlCarousel from 'react-owl-carousel3';
+import Slider from "react-slick";
 import "./styles.css";
 
 
-const options = {
-    loop:true,
-    margin: 30,
-    nav:false,
-    mouseDrag: true,
-    dots: false,
-    autoplay: true,
-    smartSpeed:1500,
-    autoplayHoverPause: true,
-    center: true,
-    responsive:{
-        0:{
-            items:1,
+const settings = {
+    dots: true,
+    infinite: true,
+    draggable: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1350,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
         },
-        768:{
-            items:2,
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
         },
-        992:{
-            items:3,
+        {
+          breakpoint: 675,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-    }
+      ]
 };
 
 const Testimonials = () => {
@@ -41,10 +53,7 @@ const Testimonials = () => {
 					<p>We have recently launched in the past weeks therefore we have very few if any reviews at the current moment. Stay tuned for future updates & posts once we gain more traction & feedback!</p>
 				</div>
 
-                {display ? <OwlCarousel 
-                    className="client-wrap owl-carousel owl-theme"
-                    {...options}
-                > 
+                {display ? <Slider className="client-wrap owl-carousel owl-theme" {...settings}>
                     <div className="single-client">
                         <i className="quotes bx bxs-quote-alt-left"></i>
                         <p>No current or pending reviews - Stay tuned for future reviews as we just launching & building an audience...</p>
@@ -120,7 +129,7 @@ const Testimonials = () => {
                             <span>Developer</span>
                         </div>
                     </div>
-                </OwlCarousel> : ''}
+                </Slider> : ''}
             </div>
         </section>
     )
