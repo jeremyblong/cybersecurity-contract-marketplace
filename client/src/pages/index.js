07933a33a2bb9621adba578b333e1507ad962fb5
Jeremy Blong <jeremyblong@gmail.com>
2022-05-14 16:11:11 -0700

    added 'subscription modal homepage' addition to request a 'beta' code

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/_App/Navbar';
 import MainBanner from '../components/HomeOne/MainBanner';
 import Partner from '../components/Common/Partner';
@@ -11,10 +11,87 @@ import Testimonials from '../components/Common/Testimonials';
 import CyberSecurityOperation from '../components/HomeOne/CyberSecurityOperation';
 import LatesNews from '../components/Common/LatesNews';
 import Footer from '../components/_App/Footer';
+import Modal from 'react-modal';
+import "./styles.css";
+import { FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText, Row, Col, Container } from 'reactstrap';
+
+
+const customStyles = {
+    content: {
+        right: 22.5,
+        left: "auto",
+        top: "auto",
+        bottom: 45,
+        position: "fixed",
+        minWidth: "82.5vw",
+        maxWidth: "82.5vw",
+        minHeight: "375px",
+        maxHeight: "375px",
+        overflowY: "scroll",
+        backgroundColor: "white",
+        zIndex: 999999999999999999999999
+    },
+    overlay: {
+        zIndex: 999999999999999999999999,
+        backgroundColor: "rgba(0, 0, 0, 0.425)"
+    }
+  };
 
 const Index = () => {
+    const [ modalIsOpen, setIsOpen ] = useState(true);
+    
     return ( 
-        <>
+        <div id={"lessen-index"}>
+            <Modal
+                id={"modal-subscription"}
+                isOpen={modalIsOpen}
+                onAfterOpen={() => {}}
+                onRequestClose={() => setIsOpen(false)}
+                style={customStyles}
+                contentLabel="Subscription Modal"
+            >
+
+                    <div className="modal-content-customized-subscribe">
+                        <div className="modal-header bg-secondary text-center">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                            <Container>
+                                <Row className='stretch-complete'>
+                                    <Col sm="12" md="12" lg="12" xl="12">
+                                        <h2>~ Subscribe to receive a 'beta' code ~</h2>
+                                    </Col>
+                                </Row>
+                                <hr />
+                                <Row className='stretch-complete'>
+                                    <Col sm="12" md="12" lg="12" xl="12">
+                                        <p>We will email you a 'beta' access code to use while signing up so you can access the restricted portions (90% of this software is restricted to authenticated users) of our marketplace. This can take 1-2 business days but we will send it!</p>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                        <div className="modal-body subscriptionmodalbody">
+                            <div className="row">
+                                <form
+                                    action="https://formspree.io/f/mzboyjzj"
+                                    method="POST"
+                                >
+                                    <FormGroup>
+                                        <Label>Enter your email to receive a 'beta' code</Label>
+                                        <InputGroup>
+                                            <InputGroupAddon addonType="append"><InputGroupText>{"EMAIL"}</InputGroupText></InputGroupAddon>
+                                            <Input name="email" className="form-control" type="email" placeholder="Enter your email & we'll send a code within 1-2 business days" aria-label="Enter your email & we'll send a code within 1-2 business days"/>
+                                        </InputGroup>
+                                    </FormGroup>
+                                    <div className="form-group col-md-12">
+                                        <input type="submit" className="btn btn-dark btn-lg btn-block" defaultValue="Subscribe" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>  
+        
+            </Modal>
+
+
             <Navbar />
 
             <MainBanner />
@@ -38,7 +115,7 @@ const Index = () => {
             <LatesNews />
             
             <Footer />
-        </>
+        </div>
     )
 }
 
