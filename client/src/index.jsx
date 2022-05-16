405891740fac02706d4a56283460ff126672ce4d
Jeremy Blong <jeremyblong@gmail.com>
2022-05-16 12:45:47 -0700

    add 'authentication screen' to restrict usage to unauthed users

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -45,7 +45,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { routes } from './route';
 import ConfigDB from './data/customizer/config'
 import { classes } from './data/layouts';
-import { NotificationContainer } from 'react-notifications';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 import Index from "./pages/index.js";
 import { RecoilRoot } from 'recoil';
 import SignIn from "./pages/sign-in.js";
@@ -63,12 +63,8 @@ import Pricing from "./pages/pricing.js";
 import ComingSoon from "./pages/coming-soon.js";
 import PrivacyPolicy from "./pages/privacy-policy.js";
 import Team from "./pages/team.js";
-import ServiceDetails from "./pages/service-details.js";
 import BlogGrid from "./pages/blog-grid.js";
 import BlogDetails from "./pages/blog-details.js";
-import ServicesOne from "./pages/services-one.js";
-import ServicesTwo from "./pages/services-two.js";
-import ServicesThree from "./pages/services-three.js";
 import BlogLeftSidebar from "./pages/blog-left-sidebar.js";
 import BlogRightSidebar from "./pages/blog-right-sidebar.js";
 import MountingLogicRedux from "./mountingLogicRedux.js";
@@ -77,7 +73,13 @@ import UnauthorizedAccessPage from "./pages/dashboard/unauthorized/unauthorizedA
 import CreateANewBlogPostPage from "./pages/unauthenticatedMisc/blogRelated/postNewBlogPage.js";
 import AboutCompanyDetails from "./pages/aboutCompany.js";
 import TermsConditions from "./pages/terms-conditions.js";
+import Sheet from 'react-modal-sheet';
+import { Container, Button, Row, Col, Card, CardHeader, CardBody, CardFooter, Form, FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText } from "reactstrap";
+import "./styles.css";
+import Cookies from 'universal-cookie';
 
+
+const cookies = new Cookies();
 
 require('dotenv').config();
 
@@ -89,6 +91,9 @@ const Root = (props) =>  {
   const defaultLayoutObj = classes.find(item => Object.values(item).pop(1) === 'compact-wrapper');
   const layout = localStorage.getItem('layout') ||  Object.keys(defaultLayoutObj).pop();
   const [ authenticated, setAuthenticatedStatus ] = useState(false);
+  const [ authModal, setAuthModal ] = useState(true);
+  const [ password, setPassword ] = useState("");
+  const [ permission, setPermission ] = useState(false);
 
   const refreshTokenApiRequest = () => {
 
@@ -202,6 +207,17 @@ const Root = (props) =>  {
       } 
     }
   }
+  const handleAuthAttempt = () => {
+    if (typeof password !== "undefined" && password === process.env.REACT_APP_ACCESS_CODE_BETA) {
+      setPermission(true);
+      setPassword("");
+      setAuthModal(false);
+
+      cookies.set('authenticated', true, { path: '/' });
+    } else {
+      NotificationManager.error("Password does NOT match the appropriate data and/or the correct credentials, please try again or contact us for a code!", "You code/password did NOT match!", 4750);
+    }
+  }
   const renderDashboardComponents = (path, Component) => {
     return (
       <ProtectedRoute key={path} exact path={`${process.env.PUBLIC_URL}${path}`}>
@@ -232,38 +248,74 @@ const Root = (props) =>  {
       <SimpleReactLightbox>
       <RecoilRoot>
       <NotificationContainer />
-      <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={(props) => renderComponent(Index, props)} />
-        <Route exact path="/company-details" render={(props) => renderComponent(AboutCompanyDetails, props)} />
-        <Route exact path="/sign-in" render={(props) => renderComponent(SignIn, props)} />
-        <Route exact path="/sign-up" render={(props) => renderComponent(SignUp, props)} />
-        <Route exact path="/forgot-password" render={(props) => renderComponent(ForgotPassword, props)} />
-        <Route exact path="/contact" render={(props) => renderComponent(Contact, props)} />
-        <Route exact path="/about" render={(props) => renderComponent(About, props)} />
-        <Route exact path="/frequently-asked-questions" render={(props) => renderComponent(FAQ, props)} />
-        <Route exact path="/testimonials" render={(props) => renderComponent(Testimonials, props)} />
-        <Route exact path="/pricing-before-login" render={(props) => renderComponent(Pricing, props)} />
-        <Route exact path="/coming-soon" render={(props) => renderComponent(ComingSoon, props)} />
-        <Route exact path="/privacy-policy" render={(props) => renderComponent(PrivacyPolicy, props)} />
-        <Route exact path="/team" render={(props) => renderComponent(Team, props)} />
-        {/* <Route exact path="/service-details" render={(props) => renderComponent(ServiceDetails, props)} /> */}
-        <Route exact path="/blog-main" render={(props) => renderComponent(BlogGrid, props)} />
-        <Route exact path="/blog-details/:id" render={(props) => renderComponent(BlogDetails, props)} />
-        {/* <Route exact path="/services-one" render={(props) => renderComponent(ServicesOne, props)} />
-        <Route exact path="/services-two" render={(props) => renderComponent(ServicesTwo, props)} />
-        <Route exact path="/services-three" render={(props) => renderComponent(ServicesThree, props)} /> */}
-        <Route exact path="/blog-left-sidebar" render={(props) => renderComponent(BlogLeftSidebar, props)} />
-        <Route exact path="/blog-right-sidebar" render={(props) => renderComponent(BlogRightSidebar, props)} />
-        <Route exact path="/terms-and-conditions" render={(props) => renderComponent(TermsConditions, props)} />
-        <Route exact path="/create-new-blog" render={(props) => renderComponent(CreateANewBlogPostPage, props)} />
-        <App>
-          <TransitionGroup>
-            {routes.map(({ path, Component }) => renderDashboardComponents(path, Component))}
-          </TransitionGroup>
-        </App>
-      </Switch>
-      </BrowserRouter>
+      {cookies.get('authenticated') !== "true" ? <Sheet id={"sheet-auth-auth"} disableDrag={true} isOpen={authModal} onClose={() => setAuthModal(false)}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>
+            <Container className='margin-password-input' fluid={true}>
+              <Row style={{ marginTop: "22.5px" }}>
+                  <Col sm="12" xl="12" lg="12" md="12">
+                    <Card>
+                        <CardHeader className='b-l-info b-r-info'>
+                            <h5>Enter the password you were provided - if you do not have a code, please reach out to <a style={{ color: "blue" }} href={"mailto:support@thehackermarketplace.com"}>our email</a> to request access...</h5>
+                        </CardHeader>
+                        <CardBody>
+                          <FormGroup className=" m-form__group">
+                            <Label>Enter your code/password</Label>
+                            <InputGroup>
+                              <InputGroupAddon addonType="prepend"><InputGroupText>{"PASSWORD"}</InputGroupText></InputGroupAddon>
+                              <Input onChange={(e) => setPassword(e.target.value)} value={password} className="form-control" type="text" placeholder="Enter your password..."/>
+                            </InputGroup>
+                          </FormGroup>
+                        </CardBody>
+                        <CardFooter className='b-l-info b-r-info'>
+                          <h4 className='text-left'>Enter the proper password or request one if you'd like to access our 'beta' launch/version...</h4>
+                          <hr />
+                          <Button className={"btn-square-success"} outline color={"success-2x"} style={{ width: "100%" }} onClick={() => handleAuthAttempt()}>Submit & Request Access!</Button>
+                        </CardFooter>
+                    </Card>
+                </Col>
+              </Row>
+            </Container>
+          </Sheet.Content>
+        </Sheet.Container>
+
+        <Sheet.Backdrop />
+      </Sheet> : null}
+      {permission === true || cookies.get('authenticated') === "true" ? <Fragment>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={(props) => renderComponent(Index, props)} />
+            <Route exact path="/company-details" render={(props) => renderComponent(AboutCompanyDetails, props)} />
+            <Route exact path="/sign-in" render={(props) => renderComponent(SignIn, props)} />
+            <Route exact path="/sign-up" render={(props) => renderComponent(SignUp, props)} />
+            <Route exact path="/forgot-password" render={(props) => renderComponent(ForgotPassword, props)} />
+            <Route exact path="/contact" render={(props) => renderComponent(Contact, props)} />
+            <Route exact path="/about" render={(props) => renderComponent(About, props)} />
+            <Route exact path="/frequently-asked-questions" render={(props) => renderComponent(FAQ, props)} />
+            <Route exact path="/testimonials" render={(props) => renderComponent(Testimonials, props)} />
+            <Route exact path="/pricing-before-login" render={(props) => renderComponent(Pricing, props)} />
+            <Route exact path="/coming-soon" render={(props) => renderComponent(ComingSoon, props)} />
+            <Route exact path="/privacy-policy" render={(props) => renderComponent(PrivacyPolicy, props)} />
+            <Route exact path="/team" render={(props) => renderComponent(Team, props)} />
+            {/* <Route exact path="/service-details" render={(props) => renderComponent(ServiceDetails, props)} /> */}
+            <Route exact path="/blog-main" render={(props) => renderComponent(BlogGrid, props)} />
+            <Route exact path="/blog-details/:id" render={(props) => renderComponent(BlogDetails, props)} />
+            {/* <Route exact path="/services-one" render={(props) => renderComponent(ServicesOne, props)} />
+            <Route exact path="/services-two" render={(props) => renderComponent(ServicesTwo, props)} />
+            <Route exact path="/services-three" render={(props) => renderComponent(ServicesThree, props)} /> */}
+            <Route exact path="/blog-left-sidebar" render={(props) => renderComponent(BlogLeftSidebar, props)} />
+            <Route exact path="/blog-right-sidebar" render={(props) => renderComponent(BlogRightSidebar, props)} />
+            <Route exact path="/terms-and-conditions" render={(props) => renderComponent(TermsConditions, props)} />
+            <Route exact path="/create-new-blog" render={(props) => renderComponent(CreateANewBlogPostPage, props)} />
+            <App>
+              <TransitionGroup>
+                {routes.map(({ path, Component }) => renderDashboardComponents(path, Component))}
+              </TransitionGroup>
+            </App>
+          </Switch>
+        </BrowserRouter>
+      </Fragment> : null}
       </RecoilRoot>
       </SimpleReactLightbox>
       </Provider>
